30b8eb5efb3ded34072e619df7b21ac6f4f1a413
william <william.del.rosario@iceshop.nl>
2020-08-01 01:43:57 +0200

    Serving Files Statically

--- src/app.ts
+++ src/app.ts
@@ -4,16 +4,19 @@ import bodyParser from "body-parser";
 import {adminRoutes} from "./routes/admin";
 import {shopRoutes} from "./routes/shop";
 import * as path from "path";
+import {rootDir} from "./util/path";
 
 const app = express();
+console.log(__dirname)
 
 app.use(bodyParser.urlencoded());
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use((req:Request, res: Response) => {
-    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+    res.status(404).sendFile(path.join(rootDir, 'views', '404.html'));
 });
 
 export { app }
