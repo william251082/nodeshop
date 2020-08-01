79d3aa2c50a1cbacc4c931526db006cdc52c3298
william <william.del.rosario@iceshop.nl>
2020-08-01 13:57:00 +0200

    Outputting Dynamic Content

--- src/routes/shop.ts
+++ src/routes/shop.ts
@@ -1,12 +1,11 @@
 import express, {Request, Response} from 'express';
-import * as path from "path";
-import {rootDir} from "../util/path";
 import * as adminData from './admin';
 
 const router = express.Router();
 
 router.get('/', (req:Request, res: Response) => {
-    res.render('shop');
+    const products = adminData.products;
+    res.render('shop', {prods: products, docTitle: 'Shop'});
 });
 
 
