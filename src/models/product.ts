7de610e81d172d2d5ad68696e86711ce72e17c20
william <william.del.rosario@iceshop.nl>
2020-08-09 17:30:46 +0200

    Sequlize - Defining a Model

--- src/models/product.ts
+++ src/models/product.ts
@@ -1,56 +1,21 @@
-export class Product {
-    constructor(
-        public id: string | null,
-        public price: number,
-        public title: string | null,
-        public description: string | null,
-        public imageUrl: string | null
+import {sequelize} from "../util/database";
+import Sequelize from "sequelize";
 
-    ) {
-        this.id = id;
-        this.price = price;
-        this.title = title;
-        this.description = description;
-        this.imageUrl = imageUrl;
-    }
-
-    get product_id(): string | null {
-        return this.id;
-    }
-
-    get product_price(): number | null {
-        return this.price;
-    }
-
-    get product_title(): string | null {
-        return this.title;
-    }
-
-    get product_description(): string | null {
-        return this.description;
-    }
-
-    get product_image_url(): string | null {
-        return this.imageUrl;
-    }
-
-    setId(id: string): void {
-        this.id = id;
-    }
-
-    setPrice(price: number): void {
-        this.price = price;
-    }
-
-    setTitle(title: string): void {
-        this.title = title;
-    }
-
-    setDescription(description: string): void {
-        this.description = description;
-    }
-
-    setImageUrl(imageUrl: string): void {
-        this.imageUrl = imageUrl;
-    }
-}
+const Product = sequelize.define('product', {
+    id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        allowNull: false,
+        primaryKey: true,
+    },
+    title: Sequelize.STRING,
+    description: {},
+    price: {
+        type: Sequelize.DOUBLE,
+        allowNull: false,
+    },
+    imageUrl: {
+        type: Sequelize.FALSE,
+        allowNull: false,
+    },
+});
--- src/util/database.ts
+++ src/util/database.ts
@@ -1,10 +1,9 @@
-// @ts-ignore
-import mysql from 'mysql2';
 import {dev_db_config} from "../config/dev";
+import Sequelize from "sequelize";
 
-export const pool_promise =  mysql.createPool({
-    host: dev_db_config.host,
-    user: dev_db_config.user,
-    database: dev_db_config.database,
-    password: dev_db_config.password,
-}).promise();
+// @ts-ignore
+export const sequelize =  new Sequelize(dev_db_config.database, dev_db_config.user, {
+        dialect: 'mysql',
+        host: dev_db_config.host
+    }
+);
